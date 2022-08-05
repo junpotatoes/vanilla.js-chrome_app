ce4eadd6f4b1599e4a7741520814dcac7325299e
최준호 <103482590+junpotatous@users.noreply.github.com>
2022-08-05 22:57:13 +0900

    complete&custom

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
@@ -8,3 +8,24 @@ const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
+
+// const audio = new Audio('spring.mp3');
+// audio.play();
+
+// document.getElementById('myAudio').play();
+
+
+// const resources = [
+//   {image: '0.jpg', sound: '/music/spring.mp3'},
+//   {image: '이미지경로', sound: '사운드경로'},
+//   {image: '이미지경로', sound: '사운드경로'},
+//   {image: '이미지경로', sound: '사운드경로'},
+//   {image: '이미지경로', sound: '사운드경로'},
+//   ]
+
+
+
+spring.onclick = function() {
+  spring_sound.play();
+}
+
